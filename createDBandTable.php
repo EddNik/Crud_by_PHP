48c1970f6c2262c855e97ae8a66258cf5efa47bf
EddNik <nikinna86@gmail.com>
2017-11-27 17:16:47 +0200

    create and insert whith redirect

--- createDBandTable.php
+++ createDBandTable.php
@@ -9,14 +9,11 @@
 $servername = "localhost";
 $username = "root";
 $password = "edd_14235";
-
-echo "Enter, please, your Database name and its description:";
+$databaseName = $_POST["nameDB"];
+$tableName = $_POST["nameTable"];
 
 if (isset($_POST["nameDB"]) && isset($_POST["nameTable"])) {
     //connect to MySQL and create database
-    $databaseName = $_POST["nameDB"];
-    $tableName = $_POST["nameTable"];
-
     $pdo_conn = new PDO("mysql:host=$servername", $username, $password);
     $sql_createDB = "CREATE DATABASE " .$_POST["nameDB"];
     $pdo_statement = $pdo_conn->prepare($sql_createDB);
@@ -29,18 +26,18 @@ if (isset($_POST["nameDB"]) && isset($_POST["nameTable"])) {
  firstName varchar(255) CHARACTER SET utf8 NOT NULL,lastName text CHARACTER SET utf8 NOT NULL,
  hireDate datetime NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 ";
-
-
     $pdo_statement = $use_database->prepare($sql_createTable);
     $pdo_statement->execute();
-
+    //перенаправляем на следующую страницу передавая две переменные
+    header("Location: insert_INTO.php?nameDB=".$_POST["nameDB"]."&nameTable=".$_POST["nameTable"]);
 }
 $datetime = date("Y-m-d H:i:s", time());
 $use_database = null;
 $pdo_conn = null;
 ?>
 <form action="" method="post">
-    <input type="text" name="nameDB" placeholder="name of database" required><br/>
-    <input type="text" name="nameTable" placeholder="name of table" required><br/>
-    <br/><input type="submit">
+    <input type="text" name="nameDB" placeholder="nameDB" required><br/>
+    <input type="text" name="nameTable" placeholder="nameTable" required><br/>
+    <input type="submit" value="create db and table"><br/>
+<!--    <input type="submit" formaction="insert_INTO.php" value="redirect to table"><br/>-->
 </form>
--- insert_INTO.php
+++ insert_INTO.php
@@ -6,20 +6,26 @@
  * Time: 8:36
  */
 
+
 $servername = "localhost";
 $username = "root";
 $password = "edd_14235";
-
-echo "Insert data";
-
+//получаем переменные из  createDBandTable.php
+$nameDB = $_GET["nameDB"];
+$nameTab = $_GET["nameTable"];
+//
+$dsn = "mysql:host=$servername;dbname=$nameDB";
 if (isset($_POST["firstName"]) && isset($_POST["lastName"]) && isset($_POST["hireDate"])) {
     //connect to database
-    $use_database = new PDO("mysql:host=$servername;dbname=employees", $username, $password);
+    try {
+        $use_database = new PDO($dsn, $username, $password);
+        $use_database->setAttribute(":ATTR_ERRMODE", ":ERRMODE_EXCEPTION");
+    } catch (PDOException $myExcept) {
+        echo "Подключение не удалось: <br/>" . $myExcept->getMessage();
+    }
     //insert data
-    $sql_useDB = "INSERT INTO article (firstName, lastName, hireDate) VALUES ( :firstName, :lastName, :hireDate)";
-
+    $sql_useDB = "INSERT INTO  $nameTab (firstName, lastName, hireDate) VALUES ( :firstName, :lastName, :hireDate)";
     $pdo_statement = $use_database->prepare($sql_useDB);
-    //$pdo_statement->bindValue(":id", $_POST["id"]);
     $pdo_statement->bindValue(":firstName", $_POST["firstName"]);
     $pdo_statement->bindValue(":lastName", $_POST["lastName"]);
     $pdo_statement->bindValue(":hireDate", $_POST["hireDate"]);
@@ -28,11 +34,9 @@ if (isset($_POST["firstName"]) && isset($_POST["lastName"]) && isset($_POST["hir
 $datetime = date("Y-m-d H:i:s", time());
 $use_database = null;
 ?>
-
 <form action="" method="post">
-<!--    <input type="text" name="id" placeholder="id_person" required><br/>-->
     <input type="text" name="firstName" placeholder="firstName" required><br/>
     <input type="text" name="lastName" placeholder="lastName" required><br/>
-    <input type="text" name="hireDate"  required value="<?php echo $datetime; ?>"> hire date
-    <br/><input type="submit">
+    <input type="text" name="hireDate"  required value="<?php echo $datetime; ?>"><br/>
+    <input type="submit">
 </form>
